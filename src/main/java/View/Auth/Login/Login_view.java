742abf8aa58354bcdb4b735b7f9b309f175fd94c
rvldrh <rivaldo0347t@gmail.com>
2026-05-22 08:35:47 +0700

    Add: login view and register view

--- src/main/java/View/Auth/Login/Login_view.java
+++ src/main/java/View/Auth/Login/Login_view.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package View;
+package View.Auth.Login;
 
 /**
  *
  * @author rvldrh
  */
-public class Login_View {
+public class Login_view {
     
 }
