1c2f74093c986c994bd77612c439748f1d7ac7bd
rvldrh <rivaldo0347t@gmail.com>
2026-05-22 07:58:41 +0700

    Initial Commit

---
+++ src/main/java/com/mycompany/casebase_kohisop_2/CaseBase_Kohisop_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.casebase_kohisop_2;
+
+/**
+ *
+ * @author rvldrh
+ */
+public class CaseBase_Kohisop_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
